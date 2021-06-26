5fe9d50d38840c43f1f981e1895cf720acc23414
Peter Swift <pswift1@ggc.edu>
2021-06-26 17:54:30 -0400

    completes commands class

--- controller/Commands.java
+++ controller/Commands.java
@@ -4,7 +4,7 @@
 * Course: ITEC 3860 Summer 2021   
 * Written: June 21, 2021   
 *    
-* This class – this class interprets and executes the commands
+* This class interprets and executes the commands
 */
 package controller;
 import java.util.*;
@@ -33,7 +33,41 @@ public class Commands {
 	 * * @param cmdLine - String containing the command entered by the user
 	 * * @return int - the integer for the command received  */
 	private int validateCommand​(String cmdLine) throws GameException{
-		return 1;
+		int returnCommand = 0;
+		char cmdChar = cmdLine.toUpperCase().charAt(0);
+		
+		
+		if( cmdChar == 'I' || cmdChar == 'G' || cmdChar == 'R'
+			|| cmdLine.equalsIgnoreCase("inspect")) {
+			  
+			returnCommand = 1;
+		}
+		else if(cmdChar == 'N' || cmdChar == 'S' ||
+			cmdChar == 'E' || cmdChar == 'W' ||
+			cmdChar == 'U' || cmdChar == 'D') {
+		    
+			returnCommand = 2;
+		}
+		else if(cmdChar == 'L' || cmdLine.equalsIgnoreCase("look"))  {
+			    
+			returnCommand = 3;
+		}
+		else if(cmdChar == 'B' || cmdLine.equalsIgnoreCase("backpack"))  {
+				    
+			returnCommand = 4;
+		}
+		else if(cmdChar == 'X' || cmdLine.equalsIgnoreCase("exit"))  {
+				    
+			returnCommand = EXIT_COMMAND;
+		}
+		
+		else {
+		    System.out.println("Acceptable Item Commands: I (inspect), G (get), R (remove)");
+		    System.out.println("Acceptable Item Commands: N (North), S (South), E (East), W (West), U (Up), D (Down)");
+		    System.out.println("Other Acceptable Commands: L (look), B (backpack), X (exit game)");
+		    throw new GameException("\nInvalid command. Please use only the above.");
+		}
+		return returnCommand;
 	}
 	
 	/** Method: executeCommand
@@ -43,7 +77,23 @@ public class Commands {
 	 * * @param cmd - String that contains the command entered by the user
 	 * * @return String - the response to the command  */
 	protected String executeCommand​(String cmd) throws GameException{
-		return null;
+		
+		String output = null;
+		int validated = validateCommand​(cmd);
+		
+		if (validated == 1 || validated == 4) {
+			output = itemCommand​(cmd);
+		}else if (validated == 2) {
+			output = move​(cmd);
+		}else if (validated == 3) {
+			output = player.printInventory();
+		}else if (validated == 0) {
+			output = "Exiting game.";
+		}else {
+			output = "Please input a valid command.";
+		}
+			
+		return output;
 	}
 	
 	/** Method: move
@@ -55,7 +105,21 @@ public class Commands {
 	 * * @param cmdRoom - String that contains the command entered by the user
 	 * * @return String - the new room the user is moving to  */
 	private String move​(String cmdRoom) throws GameException{
-		return null;
+		String nextRoom = null;
+		
+		char first = cmdRoom.toUpperCase().charAt(0);
+		int curRoomNo = player.getCurRoom();
+			
+		Room roomObj = new Room().retrieveByID(curRoomNo);
+			
+		int nextRmNo = roomObj.validDirection(first); 
+			
+		    roomObj.setVisited(true);
+		    roomObj.updateRoom();
+		    nextRoom = new Room().retrieveByID(nextRmNo).toString();
+		    player.setCurRoom​(nextRmNo);
+			
+		return nextRoom;
 	}
 	
 	/** Method: itemCommand
@@ -66,7 +130,37 @@ public class Commands {
 	 * * @param cmd - String that contains the command entered by the user
 	 * * @return the response to the user's command  */
 	private String itemCommand​(String cmd) throws GameException{
-		return null;
+		String response = "";
+		
+		int curRoomNo = player.getCurRoom();
+		Room curRoomObj = new Room().retrieveByID(curRoomNo);
+		
+		char first = cmd.toUpperCase().charAt(0);
+		
+		if(first == 'L') {
+		    response = "You are currently in\n" + curRoomObj.display();
+		}
+		else {
+		
+		    String[] temp = cmd.split(" ");
+		
+		    if(temp.length < 2) {
+			throw new GameException("An item name is required for these commands: G, R, I. [Commands.itemCommand()]");
+		    }	
+		}
+		
+			
+		if(first == 'G') {
+		    response = get​(cmd, curRoomObj);
+		}
+		else if(first == 'R') {
+		    response = remove​(cmd, curRoomObj);
+		}
+		else if(first == 'I') {
+		   response = lookItem​(cmd, curRoomObj);
+		}
+		
+		return response;
 	}
 	
 	/** Method: get
@@ -77,7 +171,27 @@ public class Commands {
 	 * * @param room - the room the item is being removed from
 	 * * @return String - item has been added to inventory  */
 	private String get​(String cmd, Room room) throws GameException{
-		return null;
+		String itemAdded = "";
+		
+		String[] temp = cmd.split(" ");
+		String itemName = temp[1];
+				
+		ArrayList<Item> roomItems = room.getRoomItems();		
+		
+		for(Item i : roomItems) {
+		    if(i.getItemName().equalsIgnoreCase(itemName)){  
+				
+			itemAdded = "This item was added to your inventory: " + i.toString();
+			player.addItem​(i);  
+			room.removeItem(i); 		
+		    }
+		    else {
+			itemAdded = "Item \"" + itemName + "\" is not found in this room. [Commands.get()]"
+					+ "\n(Check spelling.)";
+		    }
+		}
+		
+		return itemAdded;
 	}
 	
 	/** Method: remove
@@ -87,7 +201,26 @@ public class Commands {
 	 * * @param room - the current Room from student.student.view.Adventure
 	 * * @return String - the Item has been dropped  */
 	private String remove​(String cmd, Room room) throws GameException{
-		return null;
+		String itemRemoved = "";
+		
+		String[] temp = cmd.split(" ");
+		String itemName = temp[1];
+		
+		ArrayList<Item> playerInventory = player.getInventory();		
+		
+		for(Item i : playerInventory) {
+		    if(i.getItemName().equalsIgnoreCase(itemName)){  
+				
+			itemRemoved = "This item was removed from your inventory: " + i.toString();
+			player.removeItem​(i);  
+			room.dropItem(i); 
+		    }
+		    else {
+			itemRemoved = "Your inventory doesn't have this item: \"" + itemName
+				+ "\". [Commands.remove()] \n(Check your spelling.)";
+		    }
+		}
+		return itemRemoved;  
 	}
 	
 	/** Method: lookItem
@@ -96,7 +229,21 @@ public class Commands {
 	 * * @param room - the current Room from student.student.view.Adventure
 	 * * @return String - the String for the look command  */
 	private String lookItem​(String cmd, Room room) throws GameException{
-		return null;
+		String look = "";
+		
+		String[] temp = cmd.split(" ");
+		String itemName = temp[1];
+			
+		ArrayList<Item> roomItems = room.getRoomItems();
+		for(Item i : roomItems) {
+		    if(i.getItemName().equalsIgnoreCase(itemName)){
+			look = "Inspecting item - " + itemName + " - " + i.display();
+		    }
+		    else {
+			look = itemName + " is not in this room. [Commands.lookItem()]";
+		    }
+		} 
+		return look;
 	}
 
 }
